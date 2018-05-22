04a31755eb0e0f78a7218ab79154928858c1e92d
Badi James <badi.t.james@gmail.com>
2018-05-22 13:32:02 +1200

    Continuing work on rule refinement

--- ExtractingRuleRF.R
+++ ExtractingRuleRF.R
@@ -10,5 +10,20 @@ ruleExtraction <- function(rfobj, predictVarMat)
   # ntree$maxdepth is actually how you get the depth of the deepest tree 
   rawRules <- extractRules(treeList, predictVarMat, ntree = treeList$ntree,
                            maxdepth = ntree$maxdepth)
+  # need to know which tree each rule came from so know what oob data to 
+  # measure their accuracy on. Then ideally return a table with rules mapped
+  # to tree ids
   return(rawRules)
 }
+
+# oobData would ideally be a map of tree ids to the out of bag data for 
+# each tree
+ruleRefinement <- function(rawRules, oobData)
+{
+  # Initialise weight of each rule to 1
+  weightRules = data.frame(rawRules)
+  weightRules$weight = rep(1, nrow(weightRules))
+  # add values to frame for ranking
+  weightRules$accuracy = apply(weightRules, 1, measureRuleAcc, oobData)
+}
+
