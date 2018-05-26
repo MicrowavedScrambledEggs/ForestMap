aca42f3b8320dde9f31077b9a03ee812e19d637f
Badi James <badi.t.james@gmail.com>
2018-05-26 22:25:52 +1200

    Edited rule extraction now that I know how the packages work
    Trying to extract a raw rule set from a tree trained on the whole data gets 'Error: cannot allocate vector of size 2.0 Gb'

--- ExtractingRuleRF.R
+++ ExtractingRuleRF.R
@@ -1,15 +1,20 @@
 library(inTrees)
 library(randomForest)
 
-# predictVarMat is the data used to train the RF without the class labels
-ruleExtraction <- function(rfobj, predictVarMat)
+# predictVarMat is the data used to train the RF
+ruleExtraction <- function(rfobj, predictVarMat, classColName)
 {
+  # data without the class label
+  X = predictVarMat[ , -which(names(predictVarMat) %in% c(classColName))]
+  # class Label vector
+  classLab = subset(predictVarMat, select=c(classColName))
   treeList <- RF2List(rfobj)
   # Setting ntree and maxdepth values to ensure rule set perfectly represents
   # raw rule set so we can do our own rule trimming. Need to check if 
   # ntree$maxdepth is actually how you get the depth of the deepest tree 
-  rawRules <- extractRules(treeList, predictVarMat, ntree = treeList$ntree,
-                           maxdepth = ntree$maxdepth)
+  # Just set maxdepth to 100 for now
+  rawConditions <- extractRules(treeList, X, ntree = treeList$ntree, maxdepth = 100)
+  rawRules <- measureRule(rawConditions, X, classLab)
   # need to know which tree each rule came from so know what oob data to 
   # measure their accuracy on. Then ideally return a table with rules mapped
   # to tree ids
