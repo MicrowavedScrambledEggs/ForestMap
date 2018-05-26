6dcd6cf491c82ad05cd63402faacc791d28a7d0d
Badi James <badi.t.james@gmail.com>
2018-05-26 19:53:31 +1200

    Adjusted the read in forest cover dataframe to be usable with randomForest

--- RandomForestScript.R
+++ RandomForestScript.R
@@ -1,5 +1,17 @@
-#put covertype data into a txt file and add directory into line below.
+#read covertype data
 covertype <- read.csv("Project Resources/covtype.data/covtype.data", header=FALSE)
+# make covertype data a tidy format for randomForest
+colnames(covertype)[55] = "forestCoverType"
+# Convert forestCoverType to a factor vector as otherwise randomForest assumes you're doing regression
+covertype$forestCoverType[covertype$forestCoverType == 2] <- "Lodgepole_Pine"
+covertype$forestCoverType[covertype$forestCoverType == 1] <- "Spruce_Fir"
+covertype$forestCoverType[covertype$forestCoverType == 3] <- "Ponderosa_Pine"
+covertype$forestCoverType[covertype$forestCoverType == 4] <- "Cottonwood_Willow"
+covertype$forestCoverType[covertype$forestCoverType == 5] <- "Aspen"
+covertype$forestCoverType[covertype$forestCoverType == 6] <- "Douglas_Fir"
+covertype$forestCoverType[covertype$forestCoverType == 7] <- "Krummholz"
+covertype$forestCoverType <- as.factor(covertype$forestCoverType)
+
 library(randomForest)
 #number of variables
 mdim = 55
@@ -24,5 +36,5 @@ mtry = 5
   #minnimum node size (1 works fine)
   ndsize = 1
   cutoff <- 1:7
- output.Forest <- randomForest(covertype, ntree= jbt, mtry = 7)
+ output.Forest <- randomForest(forestCoverType ~ ., data=covertype, ntree= jbt, mtry = 7)
  
